ed0f9ba5f4552816c87cb643a54dff4562b0597e
Emmanuel-Anaba <emmaanaba29@gmail.com>
2024-05-16 00:26:52 +0100

    Initial Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,79 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import { questions } from "./questions.json";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const initial = Array(questions.length).fill({
+    touched: false,
+  });
+
+  const [answers, setAnswers] = useState(
+    JSON.parse(localStorage.getItem("answers")) || initial
+  );
+
+  const [current, setCurrent] = useState(
+    Number(localStorage.getItem("current")) || 0
+  );
+
+  const handleAnswer = (selected) => {
+    const newAnswers = [
+      ...answers.slice(0, current),
+      { answer: selected, touched: true },
+      ...answers.slice(current + 1),
+    ];
+    setAnswers(newAnswers);
+    localStorage.setItem("answers", JSON.stringify(newAnswers));
+  };
+
+  const handleChange = (i) => {
+    setCurrent(i);
+    localStorage.setItem("current", i);
+  };
+
+  const handleSubmit = () => {
+    const score = answers.filter(({ answer }) => answer).length;
+    alert(`You scored ${score} out of ${questions.length}`);
+    setCurrent(0);
+    setAnswers(initial);
+    localStorage.setItem("answers", JSON.stringify(initial));
+    localStorage.setItem("current", 0);
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="question">
+        <p
+          dangerouslySetInnerHTML={{
+            __html: `${current + 1}. ${questions[current].question}`,
+          }}></p>
+        {questions[current].options.map(({ option, correct }, i) => (
+          <button
+            key={i}
+            dangerouslySetInnerHTML={{
+              __html: `${option}`,
+            }}
+            className="bg-blue-500 py-2 rounded"
+            onClick={() => handleAnswer(correct)}></button>
+        ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="flex gap-2 flex-wrap justify-center">
+        {questions.map((_, i) => (
+          <button
+            className={`${
+              answers[i].touched ? "bg-blue-500" : "bg-blue-700"
+            } change-btn`}
+            key={i}
+            onClick={() => handleChange(i)}>
+            {i + 1}
+          </button>
+        ))}
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <button
+        className="bg-blue-700 m-auto px-3 py-1 rounded"
+        onClick={handleSubmit}>
+        Submit
+      </button>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
